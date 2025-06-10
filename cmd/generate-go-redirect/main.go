cee9eb7bba82be007f3c0adeea5005fa1a5567d9
Adam Luzsi <adamluzsi@gmail.com>
2025-06-10 22:44:15 +0200

    convert frameless back into monorepo
    Since I’m developing the project primarily on my own,
    consolidating all components in a single location offers clear benefits to me.
    
    This structure enhances maintainability by simplifying visibilty on code state with go tooling,,
    reduces the need for context switching,
    and making it easier to implement consistent design patterns across the different codebases.

--- cmd/generate-go-redirect/main.go
+++ cmd/generate-go-redirect/main.go
@@ -9,9 +9,9 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"log"
 	"net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -34,13 +34,13 @@ func Main(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("get import meta data failed: %w", err)
 	}
-	if err := generateProjectRedirects(metas); err != nil {
+	if err := generateProjectRedirects(ctx, metas); err != nil {
 		return fmt.Errorf("generate project redirects have failed: %w", err)
 	}
 	return nil
 }
 
-func generateProjectRedirects(metas []Meta) error {
+func generateProjectRedirects(ctx context.Context, metas []Meta) error {
 	const outDirEnvKey = "WEB_DIR_PATH"
 
 	domain, found, err := env.Lookup[string]("DOMAIN")
@@ -72,7 +72,8 @@ func generateProjectRedirects(metas []Meta) error {
 
 		var (
 			buf     bytes.Buffer
-			dirPath = filepath.Join(outDirPath, strings.TrimPrefix(meta.Import.Prefix, domain+"/"))
+			impPath = strings.TrimPrefix(meta.Import.Prefix, domain+"/")
+			dirPath = filepath.Join(outDirPath, impPath)
 			outPath = filepath.Join(dirPath, "index.html")
 		)
 
@@ -86,7 +87,25 @@ func generateProjectRedirects(metas []Meta) error {
 			return fmt.Errorf("writing out html failed: %w", err)
 		}
 
-		log.Println("INFO", fmt.Sprintf("%s redirect is created", meta))
+		logger.Info(ctx, "redirection created", logging.Field("import", meta.Import.Prefix))
+
+		for _, mod := range meta.Nested {
+			var (
+				modDirPath = filepath.Join(outDirPath, impPath, filepath.Join(path.Split(mod.Path)))
+				modOutPath = filepath.Join(modDirPath, "index.html")
+			)
+			if err := ensureDirectory(modDirPath); err != nil {
+				return err
+			}
+			// in Go, the nested module should have a go-import that tag points to the root project
+			// hence we write the same content there as to its parent
+			if err := os.WriteFile(modOutPath, buf.Bytes(), 0644); err != nil {
+				return fmt.Errorf("writing %s nested module's out html failed: %w", mod.Path, err)
+			}
+
+			logger.Info(ctx, "nested module's redirection created", logging.Field("import", path.Join(meta.Import.Prefix, mod.Path)))
+		}
+
 	}
 
 	return nil
@@ -103,6 +122,7 @@ func getImportTemplate() (*template.Template, error) {
 type Meta struct {
 	Import MetaImport
 	Source MetaSource
+	Nested []MetaNestedModule
 }
 
 type MetaImport struct {
@@ -113,6 +133,10 @@ type MetaImport struct {
 type MetaImportVCS struct {
 	Name     string `enum:"git,"`
 	RepoRoot *url.URL
+}
+
+type MetaNestedModule struct {
+	Path string
 }
 
 // MetaSource
@@ -138,12 +162,13 @@ type MetaSource struct {
 
 func getMetas() ([]Meta, error) {
 	type JSONDTO struct {
-		VCS              string `json:"vcs"`
-		ImportPrefix     string `json:"import-prefix"`
-		RootRepo         string `json:"root-repo"`
-		HomepageURL      string `json:"homepage"`
-		DirectoryPattern string `json:"directory-pattern"`
-		FilePattern      string `json:"file-pattern"`
+		VCS              string   `json:"vcs"`
+		ImportPrefix     string   `json:"import-prefix"`
+		RootRepo         string   `json:"root-repo"`
+		HomepageURL      string   `json:"homepage"`
+		DirectoryPattern string   `json:"directory-pattern"`
+		FilePattern      string   `json:"file-pattern"`
+		Submodules       []string `json:"submodules"`
 	}
 
 	const envKey = "IMPORTS_FILE_PATH"
@@ -209,9 +234,20 @@ func getMetas() ([]Meta, error) {
 			}
 		}
 
+		var nested []MetaNestedModule
+
+		if 0 < len(dto.Submodules) {
+			for _, modPath := range dto.Submodules {
+				nested = append(nested, MetaNestedModule{
+					Path: modPath,
+				})
+			}
+		}
+
 		metas = append(metas, Meta{
 			Import: imp,
 			Source: src,
+			Nested: nested,
 		})
 	}
 
